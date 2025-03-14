95bce8b219b0fc67ebcc7bcecd818e1b36b33bd1
Gautham <gauthamtajit@gmail.com>
2025-03-14 11:19:27 +0530

    6th March- First Java Program

---
+++ src/Tasks/Task001_Table_9.java
@@ -0,0 +1,17 @@
+package Tasks;
+
+public class Task001_Table_9 {
+    public static void main(String[] args){
+        int num=9;
+        System.out.printf("%d*1=%d\n", num,num*1);
+        System.out.printf("%d*1=%d\n", num,num*2);
+        System.out.printf("%d*1=%d\n", num,num*3);
+        System.out.printf("%d*1=%d\n", num,num*4);
+        System.out.printf("%d*1=%d\n", num,num*5);
+        System.out.printf("%d*1=%d\n", num,num*6);
+        System.out.printf("%d*1=%d\n", num,num*7);
+        System.out.printf("%d*1=%d\n", num,num*8);
+        System.out.printf("%d*1=%d\n", num,num*9);
+        System.out.printf("%d*1=%d\n", num,num*10);
+    }
+}
---
+++ src/ex_01_java_Basic/Lab034_Char_Literal.java
@@ -0,0 +1,10 @@
+package ex_01_java_Basic;
+
+public class Lab034_Char_Literal {
+    public static void main(String[] args) {
+        System.out.println("Gautham Ajit");
+
+
+
+    }
+}
